d01c996e501d76d3fbe2d6e07c1e8edaeb4d697d
Abytt <abytt5823@gmail.com>
2024-06-13 00:36:12 +0530

    Add2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="">
-      <Add></Add>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Add/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
