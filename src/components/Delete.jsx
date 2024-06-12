cebc145f74427e7f99a5619830554b540593a699
Abytt <abytt5823@gmail.com>
2024-06-13 01:31:08 +0530

    Delete 3

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,12 +1,102 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import NavBar from './NavBar'
 
 const Delete = () => {
-  return (
-    <div>
+    const [data, setdata] = useState(
+        {
 
-        
-    </div>
-  )
+            "Emp_name": "",
+
+        }
+    )
+    const deleteCourse = (id) => {
+        let input = {"_id":id}
+        axios.post("http://localhost:8080/Delete",input).then(
+            (response) => {
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("Delete")
+                } else {
+                    alert("not")
+                }
+            }
+            ).catch().finally()
+    }
+    const [result, changeresult] = useState([])
+    const InputHandler = (event) => {
+        setdata({ ...data, [event.target.name]: event.target.value })
+    }
+    const readvalue = () => {
+        console.log(data)
+        axios.post("http://localhost:8080/Delete", data).then(
+            (response) => {
+
+                changeresult(response.data)
+            }
+        ).catch(
+            (error) => {
+                console.log(error)
+            }
+        ).finally()
+    }
+    return (
+        <div>
+<NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+
+                            <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="label form-label" >Employee Name</label>
+                                <input type="text" className="input form-control" name='trainer_name' value={data.Emp_name} onChange={InputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-warning" onClick={readvalue}>Delete</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Emp_name</th>
+                                    <th scope="col">Emp_code</th>
+                                    <th scope="col">Emp_mobile</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                {
+                                    data.map(
+                                        (value, index) => {
+                                            return <tr>
+
+                                                <td>{value.Emp_name}</td>
+                                                <td>{value.Emp_code}</td>
+                                                <td>{value.Emp_mobile}</td>
+                                                <button className="btn btn-warning" onClick={()=>{deleteCourse(value._id)}}>Delete</button>
+                                            </tr>
+                                        }
+                                    )
+
+                                }
+
+
+
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Delete
