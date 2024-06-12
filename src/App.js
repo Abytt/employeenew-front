44593e41ee97a3a04c1da576d6b9f1d2894e29c6
Abytt <abytt5823@gmail.com>
2024-06-13 01:33:28 +0530

    Delete 4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-import View from './components/View';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Add from './components/Add';
-import Search from './components/Search';
+import Delete from './components/Delete';
+
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Add/>}/>
-  
-      <Route path='/View' element={<View/>}/>
-      <Route path='/Search' element={<Search/>}/>
+      <Route path='' element={<Delete/>}/>
     </Routes>
     </BrowserRouter>
   );
