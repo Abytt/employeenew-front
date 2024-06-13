7aba0e7c48020d399d2611d25ccd969adc9c3e6c
Abytt <abytt5823@gmail.com>
2024-06-13 07:46:01 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import View from './components/View';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Add from './components/Add';
 import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Add/>}/>
-  
+      <Route path='/Delete' element={<Delete/>}/>
       <Route path='/View' element={<View/>}/>
       <Route path='/Search' element={<Search/>}/>
     </Routes>
